59c02b108d2e14f615b32bf7621c9f5cae93f250
Luis Angel Quiñones Guerrero <luisangelq3673@gmail.com>
2022-10-06 18:54:10 -0500

    pokedex project to practice redux

---
+++ pokedux/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Searcher from './components/Searcher';
+
+function App() {
+  return (
+    <div className="App">
+      <Searcher />
+    </div>
+  );
+}
+
+export default App;
